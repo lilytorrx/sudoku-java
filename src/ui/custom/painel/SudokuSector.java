894ab3f73f20a691e0d68d6a44ee39a9d2aa0f6f
Lídia Torres <109477475+lilytorrx@users.noreply.github.com>
2025-05-15 11:49:24 -0300

    Criado painel secundário da aplicação

---
+++ src/ui/custom/painel/SudokuSector.java
@@ -0,0 +1,17 @@
+package ui.custom.painel;
+
+import javax.swing.JPanel;
+import java.awt.Dimension;
+import javax.swing.border.LineBorder;
+
+import static java.awt.Color.black;
+
+public class SudokuSector extends JPanel {
+    public SudokuSector() {
+        var dimension = new Dimension(170, 170);
+        this.setSize(dimension);
+        this.setPreferredSize(dimension);
+        this.setBorder(new LineBorder(black, 2, true));
+        this.setVisible(true);
+    }
+}
