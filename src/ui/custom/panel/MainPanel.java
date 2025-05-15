dc46b6e7158c365ff717cd6637b25bcd0c7a744a
Lídia Torres <109477475+lilytorrx@users.noreply.github.com>
2025-05-15 12:43:24 -0300

    Criando componentes da aplicação

--- src/ui/custom/panel/MainPanel.java
+++ src/ui/custom/panel/MainPanel.java
@@ -2,7 +2,6 @@ package ui.custom.panel;
 
 import javax.swing.JPanel;
 import java.awt.Dimension;
-import javax.swing.border.LineBorder;
 
 public class MainPanel extends JPanel{
     public MainPanel(final Dimension dimension) {
